a85be2d158c019c63ce51a916685033f96eaa8a0
nel <contextmachine@nels-MacBook-Pro.local>
2024-11-28 17:29:14 +0800

    Relax eslint rules

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,11 +1,12 @@
 "use client";
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "../types/supabase";
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // initialize supabase on client side
-let supabase;
+let supabase: SupabaseClient<Database>;
 
 if (typeof window !== "undefined") {
   supabase = createClient(supabaseURL!, supabaseAnonKey!);
